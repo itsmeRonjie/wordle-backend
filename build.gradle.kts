b032db84f480e54f1865a97a1af04a147cfb7cf1
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-15 14:33:45 +0800

    - updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("io.netty:netty-resolver-dns-native-macos:4.1.101.Final:osx-aarch_64")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
